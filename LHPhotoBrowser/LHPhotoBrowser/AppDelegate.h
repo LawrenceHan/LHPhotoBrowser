b90e12d3c778cec74237c129a24708eacac6a1fa
LawrenceHan <yangfei6565@163.com>
2016-01-11 17:20:39 +0800

    Added new files.

---
+++ LHPhotoBrowser/LHPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHPhotoBrowser
+//
+//  Created by Hanguang on 1/11/16.
+//  Copyright © 2016 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHPhotoBrowser/LHPhotoBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LHPhotoBrowser
+//
+//  Created by Hanguang on 1/11/16.
+//  Copyright © 2016 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
