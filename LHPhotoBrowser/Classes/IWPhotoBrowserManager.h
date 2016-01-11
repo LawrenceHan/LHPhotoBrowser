ff76e16a6915eea9058727b53d0e9c747b7968cd
LawrenceHan <yangfei6565@163.com>
2016-01-11 18:09:04 +0800

    Added comment.

--- LHPhotoBrowser/Classes/IWPhotoBrowserManager.h
+++ LHPhotoBrowser/Classes/IWPhotoBrowserManager.h
@@ -8,12 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+/// <#Description#>
 @class IWPhotoBrowserManager;
 
 @protocol IWPhotoBrowserManagerDelegate <NSObject>
 
 @optional
 
+/**
+ *  <#Description#>
+ *
+ *  @param mamanger <#mamanger description#>
+ *  @param indexes  <#indexes description#>
+ */
 - (void)photoManager:(IWPhotoBrowserManager *)mamanger deletePhotosAtIndexPaths:(NSArray <NSIndexPath *> *)indexes;
 
 @end
@@ -22,8 +29,19 @@
 @property (nonatomic, weak) id <IWPhotoBrowserManagerDelegate> delegate;
 @property (nonatomic) BOOL showAssets;
 
+/**
+ *  <#Description#>
+ *
+ *  @return <#return value description#>
+ */
 + (instancetype)sharedInstance;
 
+/**
+ *  <#Description#>
+ *
+ *  @param controller <#controller description#>
+ *  @param modally    <#modally description#>
+ */
 - (void)showPhotoBrowserInViewController:(UIViewController *)controller modally:(BOOL)modally;
 
 @end
